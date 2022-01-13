5ad5647119b17308af8bc1d5ed3620a66e1f12c7
sublatelog <blueganz@gmail.com>
2022-01-13 14:54:44 +0900

    Update upernet_swin-t_livecell.py

--- configs/seg/upernet_swin-t_livecell.py
+++ configs/seg/upernet_swin-t_livecell.py
@@ -57,7 +57,7 @@ model = dict(
 )
 
 dataset_type = 'GTBBoxDataset'
-data_root = '/data/LIVECell_dataset_2021/'
+data_root = 'data/LIVECell_dataset_2021/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True
 )
