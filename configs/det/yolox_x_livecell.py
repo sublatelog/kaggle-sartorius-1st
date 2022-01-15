f76dc0940d5b6cabfcd7fe5015beb2e9bc70705c
sublatelog <blueganz@gmail.com>
2022-01-15 20:23:55 +0900

    Update yolox_x_livecell.py

--- configs/det/yolox_x_livecell.py
+++ configs/det/yolox_x_livecell.py
@@ -110,8 +110,8 @@ test_pipeline = [
 ]
 
 data = dict(
-    samples_per_gpu=2, # 4,
-    workers_per_gpu=2, # 4,
+    samples_per_gpu=1, # 2, # 4,
+    workers_per_gpu=1, # 2, # 4,
     # persistent_workers=True,
     train=train_dataset,
     val=dict(
